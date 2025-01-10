9c0dbea5566422c63f2335f9d29e17b4b8becbcd
bloodyrainday <bloodyrainday@gmail.com>
2025-01-10 20:38:41 +0300

    feat: change the text in the experience section

--- src/layout/sections/experience/Experience.tsx
+++ src/layout/sections/experience/Experience.tsx
@@ -9,22 +9,22 @@ const experience = [
   {
     id: 0,
     year: "2022",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.",
+    text: "Began my journey into web development by enrolling in the online Rolling Scope School. Acquired foundational skills in HTML, CSS, and basic JavaScript, while completing hands-on projects to solidify my understanding.",
   },
   {
     id: 1,
     year: "2023",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.",
+    text: "Continued self-directed learning by leveraging online resources such as YouTube tutorials and coding challenges. Focused on deepening my JavaScript knowledge and honing my problem-solving skills.",
   },
   {
     id: 2,
     year: "2024",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.",
+    text: "Expanded my technical skill set by learning advanced technologies like React and TypeScript. Enrolled in the online IT-Incubator course to refine my frontend development expertise through structured learning and practical applications.",
   },
   {
     id: 3,
     year: "2025",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.",
+    text: "Successfully completed the IT-Incubator course, mastering core concepts of frontend development. Enhanced my skill set further by diving into Redux Toolkit and applying it to manage complex application states.",
   },
 ];
 
